1025e9e42a77e092599b93b4443619c8c31404ae
dashaun <dashaun@dashaun.com>
2022-12-06 11:58:21 -0600

    update test

--- src/test/java/dev/dashaun/service/gateway/ApplicationTests.java
+++ src/test/java/dev/dashaun/service/gateway/ApplicationTests.java
@@ -1,13 +1,34 @@
 package dev.dashaun.service.gateway;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.Map;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+	@LocalServerPort
+	int port;
+	private WebTestClient client;
+
+	@BeforeEach
+	public void setup() {
+		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
+	}
+
 	@Test
-	void contextLoads() {
+	@SuppressWarnings("unchecked")
+	public void pathRouteWorks() {
+		client.get().uri("/actuator/health")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(Map.class)
+				.consumeWith(result -> assertThat(result.getResponseBody()).isNotEmpty());
 	}
 
 }
